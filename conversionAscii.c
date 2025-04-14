2e2485453495b431d46892e38baccd8b3414cb72
Victor-Gonzalez-Nahuat <a24216377@alumnos.uady.mx>
2025-04-13 19:04:37 -0600

    Subir archivos principales

---
+++ conversionAscii.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "funciones.h"
+
+char** convertir_a_ascii(unsigned char* imagen, int ancho, int alto) {
+
+    printf("Implementar conversión a ASCII aquí\n");
+
+}
---
+++ guardado.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "funciones.h"
+
+void guardar_ascii(char** arte_ascii, int ancho, int alto, const char* ruta_salida) {
+
+    printf("Implementar guardado aqui\n");
+}
