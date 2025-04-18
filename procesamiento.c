33bdc3816fc3b77a56410af15845883ea731fdf7
kevin-canto <A24216386@alumnos.uady.mx>
2025-04-17 18:55:32 -0600

    procesamiento listo

--- procesamiento.c
+++ procesamiento.c
@@ -20,4 +20,21 @@ void convertir_a_grises(unsigned char* imagen, int ancho, int alto, int nuevo_an
         gris[i] = (unsigned char)(0.3 * r + 0.59 * g + 0.11 * b); //formula para copnvertir a escala de grises
     }
 
+    unsigned char* escalado = (unsigned char*)malloc(nuevo_ancho * nuevo_alto);
+
+    float proporcion_en_x = (float)ancho / nuevo_ancho; //Cuantos pixeles originales corresponden a un pixel nuevo
+    float proporcion_en_y = (float)alto / nuevo_alto; 
+
+    for (int y = 0; y < nuevo_alto; y++){
+        for (int x = 0; x < nuevo_ancho; x++){
+            int x_original = (int)(x * proporcion_en_x);
+            int y_original = (int)(y * proporcion_en_y);
+            escalado[y * nuevo_ancho + x] = gris[y_original * ancho + x_original];
+        }
+    }
+    free(gris); //liberar la imagen gris original sin escalar que no necesitamos
+
+    return escalado;
+
+
 }
